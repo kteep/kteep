ad62b177ad7e8dcdcd3cf4315e142e462d6c003b
Patrick Njoli <36375033+ogmaro@users.noreply.github.com>
2020-03-26 16:29:19 +0100

    Fixed bug, and minor changes
    Most of my changes were explained in the comments

--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
-﻿using System;
+using System;
 
-namespace conditionals
+namespace hospitalAPP
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string FirstName = "";
-            string MiddleName = "";
-            string LastName = "";
-            string Gender = "";
-            int Age = 0;
-            int BloodPressure = 0;
-            int Temperature = 0;
+            //unKnown values
+            string FirstName;
+            string MiddleName;
+            string LastName;
+            string Gender;
+            int Age;
+            int BloodPressure;
+            int Temperature;
+
+            //Known values
+            //these declared values represent the values we know such as the age, bloodpressur(bp) and tempreture(temp)'s limit.
+            //it is declared here so the parameters could easily be changed
+            int ageMargin = 50;
+            int bpMargin = 130;
+            int tempMargin = 37;
 
             Console.Write("Please enter First Name: ");
             FirstName = Console.ReadLine();
@@ -35,19 +43,34 @@ namespace conditionals
             Console.Write("Enter your Temperature: ");
             Temperature = int.Parse(Console.ReadLine());
 
-            if (Age <= 50 && BloodPressure <= 130 && Temperature <= 37)
+            //removed the equal sign here because i have reason to belive it is a bug
+            // the first argument that is equal or lesser than in line 49 and is greater than or equal in line 57
+            // it wouldn't test line 57 if line 47 is true
+            if (Age < ageMargin && BloodPressure < bpMargin && Temperature < tempMargin)
             {
-                Console.WriteLine("You are Healthy");
+                Console.WriteLine("Patient Diagnosis...");
+                Console.WriteLine("Name: " + LastName + " " + FirstName + " " + MiddleName + " ");
+                Console.WriteLine("Age: " + Age + "yrs old");
+                Console.WriteLine("Gender: " + Gender);
+                Console.WriteLine("Status: You are Healthy");
             }
-            else if(Age >= 50 && BloodPressure >= 130 && Temperature >= 37)
+            else if(Age >= ageMargin && BloodPressure >= bpMargin && Temperature >= tempMargin)
             {
-                Console.WriteLine("You will need to take some medication to keep you well");
+                Console.WriteLine("Patient Diagnosis...");
+                Console.WriteLine("Name: " + LastName + " " + FirstName + " " + MiddleName + " ");
+                Console.WriteLine("Age: " + Age + "yrs old");
+                Console.WriteLine("Gender: " + Gender);
+                Console.WriteLine("Your Health is in a Critical state, We will have to admit you immediately");
             } 
             else
             {
-                Console.WriteLine("Your Health is in a Critical state, We will have to admit you immediately");
+                Console.WriteLine("Patient Diagnosis...");
+                Console.WriteLine("Name: " + LastName + " " + FirstName + " " + MiddleName + " ");
+                Console.WriteLine("Age: " + Age + "yrs old");
+                Console.WriteLine("Gender: " + Gender);
+                Console.WriteLine("You will need to take some medication to keep you well");
             }
         }
-        
     }
 }
+
